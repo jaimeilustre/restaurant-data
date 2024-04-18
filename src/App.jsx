d57ef8980937bd0a860da0ca5e22523a9e9d0b59
Jaime Ilustre <jaimeilustre@yahoo.com>
2024-04-18 17:51:23 +0200

    changed path to restaurant list page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/:postcode" element={<RestaurantListPage />} />
+      <Route path="/restaurants/:postcode" element={<RestaurantListPage />} />
     </Routes>
     </>
   )

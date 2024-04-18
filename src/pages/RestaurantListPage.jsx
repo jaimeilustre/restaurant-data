e730c7e3f64506c437c5b148094176e193e65da4
Jaime Ilustre <jaimeilustre@yahoo.com>
2024-04-18 08:59:56 +0200

    fetching API data using axios and GET request

--- src/pages/RestaurantListPage.jsx
+++ src/pages/RestaurantListPage.jsx
@@ -1,6 +1,42 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function RestaurantListPage() {
+
+const apiUrl = import.meta.env.VITE_API_URL
+const [restaurants, setRestaurants] = useState([])
+
+// Fetching data from API
+const getApiData = () => {
+    axios.get(apiUrl)
+    .then(response => {
+        setRestaurants(response.data)
+        console.log(response.data)
+    })
+    .catch(error => {
+        console.log("Error not receiving data")
+        console.log(error)
+    })
+}
+
+useEffect(() => {
+    getApiData()
+}, [])
+
     return (
-        <h1>Hello world!</h1>
+        <>
+        {restaurants === null
+        ? <p>Loading...</p>
+        : restaurants.map(restaurant => {
+            return(
+                <div>
+                    <h1>{restaurant.metaData.district}</h1>
+                </div>
+            )
+        })
+    }
+        </>
+    
     )
 }
 

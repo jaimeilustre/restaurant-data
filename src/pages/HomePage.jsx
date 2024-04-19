4da1f14235e37ce6da86a2cd2febdd6fdce1f02a
Jaime Ilustre <jaimeilustre@yahoo.com>
2024-04-19 11:25:59 +0200

    added a search function to filter restaurants by postcode

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 function HomePage () {
+
+const [searchPostcode, setSearchPostcode] = useState('')
+
+const searchHandler = (e) => {
+    setSearchPostcode(e.target.value)
+}
+
     return (
         <>
-            <h1>Search by Postcode</h1>
+            <h1>Search restaurants in your area</h1>
+            <input
+                type="text"
+                value={searchPostcode}
+                onChange={searchHandler}
+                placeholder="Type postcode here..."
+            />
+
+            <Link to={`restaurants/${searchPostcode}`}>
+                <button>Search</button>
+            </Link>
         </>
     ) 
 }
